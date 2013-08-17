e166012d683d1015370c3a042df6afed69202818
Benoit Richter <benoit.richter@opendatasoft.com>
2013-08-17 21:46:19 +0200

    Good positioning and resizing

--- app/scripts/directives/geotooltip.js
+++ app/scripts/directives/geotooltip.js
@@ -2,35 +2,48 @@
 
 angular.module('ngGeotooltipApp')
 	.directive('geotooltip', ['$timeout', '$window', function ($timeout, $window) {
+		// The container is shared between directives to avoid performance issues
 		var container = angular.element('<div id="geotooltip"></div>');
-		angular.element($window.document.body).append(container);
 		var map = null;
 		var layer = null;
 		var displayed = false;
 
-		var displayTooltip = function(coords) {
-			return function() {
-				if (!displayed) {
-					displayed = true;
-					if (map == null) {
-						map = new L.map(container[0]);
-						var tilesUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png';
-		                var attrib = 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"> - Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors';
-		                var tileLayer = new L.TileLayer(tilesUrl, {minZoom: 1, maxZoom: 18, attribution: attrib, subdomains: "1234"});
-		                map.addLayer(tileLayer);
-		            }
+		var displayTooltip = function(tippedElement, coords, width, height) {
+			if (!displayed) {
+				displayed = true;
 
-	                var point = new L.LatLng(coords[0], coords[1]);
-	                map.setView(point, 12);
+				// Make the container the right size
+				var resized = false;
+				if (width !== container.css('width') || height !== container.css('height')) {
+					resized = true;
+				}
+				container.css('width', width);
+				container.css('height', height);
 
-	                if (layer !== null) {
-	                	map.removeLayer(layer);
-	                }
-	                layer = L.marker(point).addTo(map);
-	                container.css('visibility', 'visible');
-	                console.log(container);
+				// Position it at the right place
+				container.css('top', tippedElement.prop('offsetTop')+tippedElement.prop('offsetHeight')+'px');
+				container.css('left', tippedElement.prop('offsetLeft')+tippedElement.prop('offsetWidth')+'px');
+				tippedElement.after(container);
+				
+				if (map == null) {
+					map = new L.map(container[0]);
+					var tilesUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png';
+	                var attrib = 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"> - Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors';
+	                var tileLayer = new L.TileLayer(tilesUrl, {minZoom: 1, maxZoom: 18, attribution: attrib, subdomains: "1234"});
+	                map.addLayer(tileLayer);
+	            } else if (resized) {
+	            	map.invalidateSize();
 	            }
-			}
+
+                var point = new L.LatLng(coords[0], coords[1]);
+                map.setView(point, 12);
+
+                if (layer !== null) {
+                	map.removeLayer(layer);
+                }
+                layer = L.marker(point).addTo(map);
+                container.css('visibility', 'visible');
+            }
 		};
 
 		var hideTooltip = function() {
@@ -44,15 +57,29 @@ angular.module('ngGeotooltipApp')
 			restrict: 'E',
 			transclude: true,
 			scope: {
-				'coords': '='
+				'coords': '=',
+				'width': '@',
+				'height': '@'
 			},
 			link: function(scope, element, attrs) {
 				var tooltipPop = null;
+				var tooltipWidth = '200px';
+				var tooltipHeight = '200px';
+				if (attrs.height) {
+					tooltipHeight = attrs.height+'px';
+				}
+				if (attrs.width) {
+					tooltipWidth = attrs.width+'px';
+				}
+
+				// Events
 				element.bind('mouseenter', function(e) {
-					tooltipPop = $timeout(displayTooltip(scope.coords), 1000).then(function() { tooltipPop = null; });
+					tooltipPop = $timeout(function() {
+						displayTooltip(element, scope.coords, tooltipWidth, tooltipHeight);
+					}, 1000).then(function() { tooltipPop = null; });
 				});
 				element.bind('click', function(e) {
-					displayTooltip(scope.coords)();
+					displayTooltip(element, scope.coords, tooltipWidth, tooltipHeight);
 					if (tooltipPop !== null) {
 						// Chances are we triggered the original timer
 						$timeout.cancel(tooltipPop);
