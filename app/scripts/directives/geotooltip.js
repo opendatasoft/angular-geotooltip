b737d73ffab361005c1167e41ab2d0b032b689cf
Benoit Richter <benoit.richter@opendatasoft.com>
2013-08-17 22:27:28 +0200

    Safer display mode, but there are still ghosts...

--- app/scripts/directives/geotooltip.js
+++ app/scripts/directives/geotooltip.js
@@ -6,62 +6,56 @@ angular.module('ngGeotooltipApp')
 		var container = angular.element('<div id="geotooltip" style="visibility: hidden; position: absolute;"></div>');
 		var map = null;
 		var layerGroup = null;
-		var displayed = false;
-
+		
 		var displayTooltip = function(tippedElement, width, height, coords, geoJson) {
-			if (!displayed) {
-				displayed = true;
+			// Make the container the right size
+			var resized = false;
+			if (width !== container.css('width') || height !== container.css('height')) {
+				resized = true;
+			}
+			container.css('width', width);
+			container.css('height', height);
 
-				// Make the container the right size
-				var resized = false;
-				if (width !== container.css('width') || height !== container.css('height')) {
-					resized = true;
-				}
-				container.css('width', width);
-				container.css('height', height);
-
-				// Position it at the right place
-				container.css('top', tippedElement.prop('offsetTop')+tippedElement.prop('offsetHeight')+'px');
-				container.css('left', tippedElement.prop('offsetLeft')+tippedElement.prop('offsetWidth')+'px');
-				tippedElement.after(container);
-				
-				if (map == null) {
-					map = new L.map(container[0]);
-					var tilesUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png';
-	                var attrib = 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"> - Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors';
-	                var tileLayer = new L.TileLayer(tilesUrl, {minZoom: 1, maxZoom: 18, attribution: attrib, subdomains: "1234"});
-	                map.addLayer(tileLayer);
-	            } else if (resized) {
-	            	map.invalidateSize();
-	            }
-
-                if (layerGroup !== null) {
-                	map.removeLayer(layerGroup);
-                }
-                layerGroup = L.layerGroup();
-                var bounds = new L.LatLngBounds();
-
-                if (coords) {
-                	var point = new L.LatLng(coords[0], coords[1]);
-                	var pointLayer = L.marker(point);
-                	layerGroup.addLayer(pointLayer);
-                	bounds.extend(point);
-                }
-
-                if (geoJson) {
-                	var geoJsonLayer = L.geoJson(geoJson);
-                	layerGroup.addLayer(geoJsonLayer);
-                	bounds.extend(geoJsonLayer.getBounds());
-                }
-                //map.setView(point, 12);
-                layerGroup.addTo(map);
-                map.fitBounds(bounds);
-                container.css('visibility', 'visible');
+			// Position it at the right place
+			container.css('top', tippedElement.prop('offsetTop')+tippedElement.prop('offsetHeight')+'px');
+			container.css('left', tippedElement.prop('offsetLeft')+tippedElement.prop('offsetWidth')+'px');
+			tippedElement.after(container);
+			
+			if (map == null) {
+				map = new L.map(container[0]);
+				var tilesUrl = 'http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png';
+                var attrib = 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png"> - Map data © <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors';
+                var tileLayer = new L.TileLayer(tilesUrl, {minZoom: 1, maxZoom: 18, attribution: attrib, subdomains: "1234"});
+                map.addLayer(tileLayer);
+            } else if (resized) {
+            	map.invalidateSize();
             }
+
+            if (layerGroup !== null) {
+            	map.removeLayer(layerGroup);
+            }
+            layerGroup = L.layerGroup();
+            var bounds = new L.LatLngBounds();
+
+            if (coords) {
+            	var point = new L.LatLng(coords[0], coords[1]);
+            	var pointLayer = L.marker(point);
+            	layerGroup.addLayer(pointLayer);
+            	bounds.extend(point);
+            }
+
+            if (geoJson) {
+            	var geoJsonLayer = L.geoJson(geoJson);
+            	layerGroup.addLayer(geoJsonLayer);
+            	bounds.extend(geoJsonLayer.getBounds());
+            }
+            //map.setView(point, 12);
+            layerGroup.addTo(map);
+            map.fitBounds(bounds);
+            container.css('visibility', 'visible');
 		};
 
 		var hideTooltip = function() {
-			displayed = false;
 			container.css('visibility', 'hidden');
 		}
 
@@ -81,7 +75,7 @@ angular.module('ngGeotooltipApp')
 				var tooltipPop = null;
 				var tooltipWidth = '200px';
 				var tooltipHeight = '200px';
-				var delay = attrs.delay || 1000
+				var delay = attrs.delay || 1000;
 				if (attrs.height) {
 					tooltipHeight = attrs.height+'px';
 				}
@@ -103,9 +97,8 @@ angular.module('ngGeotooltipApp')
 					}
 				})
 				element.bind('mouseleave', function(e) {
-					if (tooltipPop === null) {
-						hideTooltip();
-					} else {
+					hideTooltip();
+					if (tooltipPop !== null) {
 						// We are currently counting down until the tooltip appearance, let's forget it
 						$timeout.cancel(tooltipPop);
 					}
