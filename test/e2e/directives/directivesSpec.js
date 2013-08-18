ce584fd6a5bf1ec3807b7ff331403dbb63d08b46
Benoit Richter <benoit.richter@opendatasoft.com>
2013-08-18 20:16:03 +0200

    Some E2E tests

--- test/e2e/directives/directivesSpec.js
+++ test/e2e/directives/directivesSpec.js
@@ -9,18 +9,31 @@ describe("E2E: Testing geotooltip directive in demo page", function() {
 	});
 
 	it('should change the style of the tooltipped text', function() {
+		expect(element('span.geotooltip').css('cursor')).toBe('help');
+		expect(element('span.geotooltip').css('border-bottom-style')).toBe('dotted');
+	});
+
+	it('should appear instantly if there is a delay but I click on the text, and be located in the bottom-right corner of the directive', function() {
+		element('#case1').click();
+		// Is the tooltip in the DOM?
+		expect(element('#geotooltip').count()).toBe(1);
+		// Is the tooltip visible?
+		expect(element('#geotooltip:visible').count()).toBe(1);
+	})
+
+	// it('should make a tooltip appear instantly if there is a 0 delay', function() {
 		
-	});
+	// });
 
-	it('should make a tooltip appear instantly in the bottom-right corner of the directive if there is a 0 delay', function() {
+	// it('should make a tooltip appear after one second by default', function() {
 
-	});
+	// });
 
-	it('should make a tooltip appear after one second by default', function() {
+	// it('should make a tooltip appear after half a second', function() {
 
-	});
+	// });
 
-	it('should make a tooltip appear after half a second', function() {
+	// it('should not make the tooltip appear if I move my mouse outside before the delay', function() {
 
-	});
+	// });
 });
